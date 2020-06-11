30a8d827bea6b1facf6e61b8c95847a82041c7a8
Vikash Kumar <vikkumar@iu.edu>
2020-06-10 20:56:20 -0400

    Updates

---
+++ precache-manifest.e1a926b2ebf2c0e547d209d127a8e8f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387d33457551dd4b11a7e5b253a11e57",
+    "url": "/Monster-Website/index.html"
+  },
+  {
+    "revision": "21275f5379ce5ce53c77",
+    "url": "/Monster-Website/static/css/main.4d657ab1.chunk.css"
+  },
+  {
+    "revision": "d58e09b5d14c6aee0ca0",
+    "url": "/Monster-Website/static/js/2.f37b6786.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Website/static/js/2.f37b6786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21275f5379ce5ce53c77",
+    "url": "/Monster-Website/static/js/main.bb30b997.chunk.js"
+  },
+  {
+    "revision": "c929b140baf9cad5333a",
+    "url": "/Monster-Website/static/js/runtime-main.04ec429d.js"
+  }
+]);
